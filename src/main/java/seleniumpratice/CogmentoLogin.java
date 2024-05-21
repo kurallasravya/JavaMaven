5d1c6c3053706a9414e6ab2b16cb4a864349ea0a
sravya <kurallasravya@gmail.com>
2024-05-21 16:12:20 +0530

    jj

--- src/main/java/seleniumpratice/CogmentoLogin.java
+++ src/main/java/seleniumpratice/CogmentoLogin.java
@@ -33,10 +33,26 @@ public class CogmentoLogin {
        driver.findElement(By.xpath("//label[normalize-space()='Phone']")).click();
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("document.body.style.zoom='0.67'");
-        driver.findElement(By.xpath("//label[text()='Phone']/following-sibling::div//i[@class='dropdown icon']")).click();
-        //driver.findElement(By.xpath("//div[@name='hint']//input[@type='text']")).click();
-       driver.findElement(By.xpath("//div[contains(text(),'India')]")).click();
-        //123
+//        driver.findElement(By.xpath("//label[text()='Phone']/following-sibling::div//i[@class='dropdown icon']")).se;
+//        //driver.findElement(By.xpath("//div[@name='hint']//input[@type='text']")).click();
+//       driver.findElement(By.xpath("//div[contains(text(),'India')]")).click();
+//        //123
+        WebElement streetAddress = driver.findElement(By.xpath("//input[@name='address']"));
+        streetAddress.sendKeys("Bhel");
+        WebElement city = driver.findElement(By.xpath("//input[@placeholder='City']"));
+        city.sendKeys("Hyderabad");
+//        WebElement state = driver.findElement(By.cssSelector("input[placeholder='State / County']"));
+//        state.sendKeys("Telangana");
+//        WebElement post = driver.findElement(By.xpath("//input[@name='zip']"));
+//        post.sendKeys("502032");
+//        div[@name='country']//input[@type='text']//div[@name='country']//i[@class='dropdown icon']//div[@class='fields']//div[100]
+//     driver.findElement(By.xpath("//div[@id='main-content']/div/div[2]/form/div[3]/div/div/div/div/div/div/input\n")).click();
+//     driver.findElement(By.xpath("//span[(text() = 'United Arab Emirates' or . = 'United Arab Emirates')]")).sendKeys("United Arab Emirates");
+        WebElement email = mywait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Email address']")));
+        email.sendKeys("kurallasravya@gmail.com");
+       WebElement Industry = driver.findElement(By.xpath("//label[text()='Industry']/parent::div//input"));
+       Industry.click();
+
 
 
 
